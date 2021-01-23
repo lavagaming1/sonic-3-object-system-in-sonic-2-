55e7799db7ee4b16a6c4d198608ac5e876e1b715
RandomName-proj <2012ame2012@gmail.com>
2021-01-23 20:36:10 +0300

    Import everything

---
+++ _inc/src/skcompat.asm
@@ -0,0 +1,54 @@
+; ===========================================================================
+; Copyright (C) 2011-2018 by flamewing
+;
+; Permission to use, copy, modify, and/or distribute this software for any
+; purpose with or without fee is hereby granted.
+;
+; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+; WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+; MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+; ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+; WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+; ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+; OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+; ===========================================================================
+; A few constants
+palette_line_0      =      (0<<13)
+palette_line_1      =      (1<<13)
+palette_line_2      =      (2<<13)
+palette_line_3      =      (3<<13)
+tile_mask           =      $07FF
+
+; Remapped function names
+KosDec EQU Kos_Decomp
+EniDec EQU Eni_Decomp
+
+; Remapped RAM locations
+Chunk_Table EQU Chunk_table
+System_Stack EQU System_stack
+
+; VRAM constants
+VRAM_Plane_A_Name_Table               = $C000	; Extends until $CFFF
+ArtTile_VRAM_Start                    = $0000
+
+
+; simplifying macros and functions
+
+; makes a VDP address difference
+vdpCommDelta function addr,((addr&$3FFF)<<16)|((addr&$C000)>>14)
+
+; macros to convert from tile index to art tiles, block mapping or VRAM address.
+make_art_tile function addr,pal,pri,((pri&1)<<15)|((pal&3)<<13)|(addr&tile_mask)
+tiles_to_bytes function addr,((addr&$7FF)<<5)
+
+; macro to declare an offset table
+offsetTable macro {INTLABEL}
+current_offset_table := __LABEL__
+__LABEL__ label *
+    endm
+
+; macro to declare an entry in an offset table
+offsetTableEntry macro ptr
+	dc.ATTRIBUTE ptr-current_offset_table
+    endm
+
---
+++ art/dplc/DPLC - Fireworm.asm
@@ -0,0 +1,11 @@
+; --------------------------------------------------------------------------------
+; Dynamic Pattern Loading Cues - output from SonMapEd - Sonic 2 format; --------------------------------------------------------------------------------
+
+SME_9HQ5f:	
+		dc.w SME_9HQ5f_8-SME_9HQ5f, SME_9HQ5f_A-SME_9HQ5f	
+		dc.w SME_9HQ5f_C-SME_9HQ5f, SME_9HQ5f_E-SME_9HQ5f	
+SME_9HQ5f_8:	dc.b 0, 0	
+SME_9HQ5f_A:	dc.b 0, 0	
+SME_9HQ5f_C:	dc.b 0, 0	
+SME_9HQ5f_E:	dc.b 0, 0	
+		even
