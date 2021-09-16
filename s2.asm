5940317e56f85d9fb32e50d397bbe643fb1bfe8c
lavagaming1 <30357171+lavagaming1@users.noreply.github.com>
2021-09-16 18:35:29 +0300

    new priority system

--- s2.asm
+++ s2.asm
@@ -27907,28 +27907,7 @@ DeleteObject2:
 
 
 
-; ---------------------------------------------------------------------------
-; Subroutine to display a sprite/object, when a0 is the object RAM
-; ---------------------------------------------------------------------------
 
-; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
-
-; sub_164F4:
-DisplaySprite:
-	lea	(Sprite_Table_Input).w,a1
-	move.w	priority(a0),d0
-	lsr.w	#1,d0
-	andi.w	#$380,d0
-	adda.w	d0,a1
-	cmpi.w	#$7E,(a1)
-	bhs.s	return_16510
-	addq.w	#2,(a1)
-	adda.w	(a1),a1
-	move.w	a0,(a1)
-
-return_16510:
-	rts
-; End of function DisplaySprite
 Draw_Sprite:
 		lea	(Sprite_Table_Input).w,a1
 		adda.w	priority(a0),a1
@@ -27957,20 +27936,55 @@ loc_1ABDC:
 ; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
 
 ; sub_16512:
+DisplaySprite:
+        lea     (Sprite_Table_Input).w,a1
+        moveq   #0,d0
+	move.b  priority(a0),d0
+        add.w  d0,d0
+        move.w  PriorityId(pc,d0.w),d0     ; get values
+        adda.w  d0,a1
+        cmpi.w  #$7E,(a1)
+        bcc.s   return_16510
+        addq.w  #2,(a1)
+        adda.w  (a1),a1
+        move.w  a0,(a1)
+
+return_16510:
+        rts
+; End of function DisplaySprite
+PriorityId:    dc.w  0
+               dc.w  $80
+               dc.w  $100
+               dc.w  $180
+               dc.w  $200
+               dc.w  $280
+               dc.w  $300
+               dc.w  $380
+               even
+; ---------------------------------------------------------------------------
+; Subroutine to display a sprite/object, when a1 is the object RAM
+; ---------------------------------------------------------------------------
+
+; ||||||||||||||| S U B R O U T I N E |||||||||||||||||||||||||||||||||||||||
+
+; sub_16512:
 DisplaySprite2:
-	lea	(Sprite_Table_Input).w,a2
-	move.w	priority(a1),d0
-	lsr.w	#1,d0
-	andi.w	#$380,d0
-	adda.w	d0,a2
-	cmpi.w	#$7E,(a2)
-	bhs.s	return_1652E
-	addq.w	#2,(a2)
-	adda.w	(a2),a2
-	move.w	a1,(a2)
+        lea     (Sprite_Table_Input).w,a2
+        moveq   #0,d0
+	move.b  priority(a1),d0
+        add.w  d0,d0
+        move.w  PriorityId(pc,d0.w),d0     ; get values
+
+        adda.w  d0,a2
+        cmpi.w  #$7E,(a2)
+        bcc.s   return_1652E
+        addq.w  #2,(a2)
+        adda.w  (a2),a2
+        move.w  a1,(a2)
 
 return_1652E:
-	rts
+        rts
+; End of function DisplaySprite2
 ; End of function DisplaySprite2
 
 ; ---------------------------------------------------------------------------
