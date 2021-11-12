ce65eb05025c3a6bd9f34068d2b11c65b679035d
lavagaming1 <30357171+lavagaming1@users.noreply.github.com>
2021-11-12 23:58:48 +0300

    uhh

--- s2.asm
+++ s2.asm
@@ -88461,6 +88461,7 @@ ColCurveMap:	BINCLUDE	"collision/Curve and resistance mapping.bin"
 ; Collision arrays
 ;--------------------------------------------------------------------------------------
 ColArray:	BINCLUDE	"collision/Collision array 1.bin"
+       even
 ColArray2:	BINCLUDE	"collision/Collision array 2.bin"
 	even
 ;---------------------------------------------------------------------------------------
@@ -88665,13 +88666,18 @@ Level_SCZ:	BINCLUDE	"level/layout/SCZ.bin"
 ; Animated flowers in EHZ and HTZ ; ArtUnc_49714: ArtUnc_49794: ArtUnc_49814: ArtUnc_49894:
 ;---------------------------------------------------------------------------------------
 ArtUnc_Flowers1:	BINCLUDE	"art/uncompressed/EHZ and HTZ flowers - 1.bin"
+         even
 ArtUnc_Flowers2:	BINCLUDE	"art/uncompressed/EHZ and HTZ flowers - 2.bin"
+        even
 ArtUnc_Flowers3:	BINCLUDE	"art/uncompressed/EHZ and HTZ flowers - 3.bin"
+        even
 ArtUnc_Flowers4:	BINCLUDE	"art/uncompressed/EHZ and HTZ flowers - 4.bin"
+        even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Pulsing thing against checkered backing from EHZ ; ArtUnc_49914:
 ArtUnc_EHZPulseBall:	BINCLUDE	"art/uncompressed/Pulsing ball against checkered background (EHZ).bin"
+         even
 ;---------------------------------------------------------------------------------------
 ; Nemesis compressed art (192 blocks)
 ; Dynamically reloaded cliffs in background from HTZ ; ArtNem_49A14: ArtUnc_HTZCliffs:
@@ -88681,18 +88687,22 @@ ArtNem_HTZCliffs:	BINCLUDE	"art/nemesis/Dynamically reloaded cliffs in HTZ backg
 ; Uncompressed art
 ; Dynamically reloaded clouds in background from HTZ ; ArtUnc_4A33E:
 ArtUnc_HTZClouds:	BINCLUDE	"art/uncompressed/Background clouds (HTZ).bin"
+       even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Spinning metal cylinder patterns in MTZ ; ArtUnc_4A73E:
 ArtUnc_MTZCylinder:	BINCLUDE	"art/uncompressed/Spinning metal cylinder (MTZ).bin"
+        even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Lava patterns in MTZ and HTZ  ; ArtUnc_4B73E:
 ArtUnc_Lava:	BINCLUDE	"art/uncompressed/Lava.bin"
+  even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Animated section of MTZ background ; ArtUnc_4BD3E:
 ArtUnc_MTZAnimBack:	BINCLUDE	"art/uncompressed/Animated section of MTZ background.bin"
+        even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Pulsing orb in HPZ
@@ -88701,34 +88711,45 @@ ArtUnc_HPZPulseOrb:	;BINCLUDE	"art/uncompressed/Pulsing orb (HPZ).bin"
 ; Uncompressed art
 ; Pulsing ball in OOZ   ; ArtUnc_4BF7E:
 ArtUnc_OOZPulseBall:	BINCLUDE	"art/uncompressed/Pulsing ball (OOZ).bin"
+        even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Square rotating around ball in OOZ ; ArtUnc_4C0FE: ArtUnc_4C2FE:
 ArtUnc_OOZSquareBall1:	BINCLUDE	"art/uncompressed/Square rotating around ball in OOZ - 1.bin"
+          even
 ArtUnc_OOZSquareBall2:	BINCLUDE	"art/uncompressed/Square rotating around ball in OOZ - 2.bin"
+        even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Oil in OOZ    ; ArtUnc_4C4FE: ArtUnc_4CCFE:
 ArtUnc_Oil1:	BINCLUDE	"art/uncompressed/Oil - 1.bin"
+       even
 ArtUnc_Oil2:	BINCLUDE	"art/uncompressed/Oil - 2.bin"
+        even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Flipping foreground section in CNZ ; ArtUnc_4D4FE:
 ArtUnc_CNZFlipTiles:	BINCLUDE	"art/uncompressed/Flipping foreground section (CNZ).bin"
+        even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Bonus pictures for slots in CNZ ; ArtUnc_4EEFE:
 ArtUnc_CNZSlotPics:	BINCLUDE	"art/uncompressed/Slot pictures.bin"
+       even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Animated background section in CPZ and DEZ ; ArtUnc_4FAFE:
 ArtUnc_CPZAnimBack:	BINCLUDE	"art/uncompressed/Animated background section (CPZ and DEZ).bin"
+      even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Waterfall patterns from ARZ   ; ArtUnc_4FCFE: ArtUnc_4FDFE: ArtUnc_4FEFE:
 ArtUnc_Waterfall1:	BINCLUDE	"art/uncompressed/ARZ waterfall patterns - 1.bin"
+  even
 ArtUnc_Waterfall2:	BINCLUDE	"art/uncompressed/ARZ waterfall patterns - 2.bin"
+    even
 ArtUnc_Waterfall3:	BINCLUDE	"art/uncompressed/ARZ waterfall patterns - 3.bin"
+     even
 ;---------------------------------------------------------------------------------------
 ; Uncompressed art
 ; Patterns for Sonic  ; ArtUnc_50000:
@@ -88746,6 +88767,7 @@ ArtUnc_Tails:	BINCLUDE	"art/uncompressed/Tails's art.bin"
 ; Sonic			; MapUnc_6FBE0: SprTbl_Sonic:
 ;--------------------------------------------------------------------------------------
 Mapunc_Sonic:	BINCLUDE	"mappings/sprite/Sonic.bin"
+        even
 ;--------------------------------------------------------------------------------------
 ; Sprite Dynamic Pattern Reloading
 ; Sonic DPLCs   		; MapRUnc_714E0:
@@ -88753,6 +88775,7 @@ Mapunc_Sonic:	BINCLUDE	"mappings/sprite/Sonic.bin"
 ; WARNING: the build script needs editing if you rename this label
 ;          or if you move Sonic's running frame to somewhere else than frame $2D
 MapRUnc_Sonic:	BINCLUDE	"mappings/spriteDPLC/Sonic.bin"
+        even
 ;--------------------------------------------------------------------------------------
 ; Nemesis compressed art (32 blocks)
 ; Shield			; ArtNem_71D8E:
@@ -88775,6 +88798,7 @@ ArtUnc_Invincible_stars:	BINCLUDE	"art/uncompressed/Invincibility stars.unc"
 ; Uncompressed art
 ; Splash in water and dust from skidding	; ArtUnc_71FFC:
 ArtUnc_SplashAndDust:	BINCLUDE	"art/uncompressed/Splash and skid dust.bin"
+       even
 ;--------------------------------------------------------------------------------------
 ; Nemesis compressed art (14 blocks)
 ; Supersonic stars		; ArtNem_7393C:
@@ -88785,6 +88809,7 @@ ArtNem_SuperSonic_stars:	BINCLUDE	"art/nemesis/Super Sonic stars.bin"
 ; Tails			; MapUnc_739E2:
 ;--------------------------------------------------------------------------------------
 MapUnc_Tails:	BINCLUDE	"mappings/sprite/Tails.bin"
+        even
 ;--------------------------------------------------------------------------------------
 ; Sprite Dynamic Pattern Reloading
 ; Tails DPLCs	; MapRUnc_7446C:
